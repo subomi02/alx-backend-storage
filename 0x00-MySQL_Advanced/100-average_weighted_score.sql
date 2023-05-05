e3a0b82a54996defe46fe2189ef9147ca654cac7
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 23:42:37 +0100

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,3 +1,4 @@
+-- Adetunji Olasubomi
 -- creates a stored procedure ComputeAverageWeightedScoreForUser that
 -- computes and store the average weighted score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
