d7fb4776656b5a233e8e52800682bff513fd09c0
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 23:42:31 +0100

    Update 10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,3 +1,4 @@
+-- Adetunji Olasubomi
 -- creates a function SafeDiv that divides (and returns) the first
 -- by the second number or returns 0 if the second number is equal to 0.
 DROP FUNCTION IF EXISTS SafeDiv;
