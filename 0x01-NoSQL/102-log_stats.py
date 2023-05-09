1c753e5683e1112b82f9ea0322a59385b5d26dd7
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-09 16:57:20 +0100

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-'''Task 15's module.
+'''Adetunji Olasubomi
 '''
 from pymongo import MongoClient
 
 
 def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
+    '''
     '''
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
@@ -20,7 +20,7 @@ def print_nginx_request_logs(nginx_collection):
 
 
 def print_top_ips(server_collection):
-    '''Prints statistics about the top 10 HTTP IPs in a collection.
+    '''
     '''
     print('IPs:')
     request_logs = server_collection.aggregate(
