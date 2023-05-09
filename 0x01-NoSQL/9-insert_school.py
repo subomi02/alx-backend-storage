e5867b936fba6ace76bd9615072b76f082edbe45
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-09 16:55:01 +0100

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''Task 9's module.
+'''Adetunji Olasubomi
 '''
 
 
 def insert_school(mongo_collection, **kwargs):
-    '''Inserts a new document in a collection.
+    '''
     '''
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
